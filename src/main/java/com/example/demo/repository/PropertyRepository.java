d53d79e45a9d959cf116b59055eb640b6a56a393
bala-amypo <bala@amypo.in>
2025-12-21 13:07:40 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/PropertyRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Property;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+    @Query("SELECT p FROM Property p WHERE p.city = :city")
+    List<Property> findByCityHql(@Param("city") String city);
+
+    List<Property> findByCity(String city);
+}
