808c8c90242db7e22860fc3f7846a837ffee943e
bala-amypo <bala@amypo.in>
2025-12-21 13:08:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/RatingResultRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.RatingResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RatingResultRepository extends JpaRepository<RatingResult, Long> {
+    RatingResult findByPropertyId(Long propertyId);
+}
