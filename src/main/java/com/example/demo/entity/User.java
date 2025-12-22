805813cf5b5cbc7e55261e65bf82d2b4c2d56e7d
bala-amypo <bala@amypo.in>
2025-12-22 01:06:55 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,28 +1,4 @@
-// package com.example.demo.entity;
 
-// import jakarta.persistence.*;
-// import lombok.*;
-
-// @Entity
-// @Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class User {
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//     private String name;
-
-//     @Column(nullable = false, unique = true)
-//     private String email;
-
-//     private String password;
-
-//     private String role = "ANALYST";
-// }
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,12 +1,4 @@
-// package com.example.demo.repository;
 
-// import com.example.demo.entity.User;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface UserRepository extends JpaRepository<User, Long> {
-//     boolean existsByEmail(String email);
-//     User findByEmail(String email);
-// }
 package com.example.demo.repository;
 
 import com.example.demo.entity.User;
