368d7a723a7c3d322f2b736452f9c873a41a04e9
bala-amypo <bala@amypo.in>
2025-12-23 09:10:39 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.ExceptionHandler;
-// import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-// @RestControllerAdvice
-// public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
-//     @ExceptionHandler(EmailAlreadyExistsException.class)
-//     public ResponseEntity<String> handleEmailExists(
-//             EmailAlreadyExistsException ex) {
-//         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-//     }
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailExists(
+            EmailAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
-//     @ExceptionHandler(InvalidCredentialsException.class)
-//     public ResponseEntity<String> handleInvalidCredentials(
-//             InvalidCredentialsException ex) {
-//         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
-//     }
-// }
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<String> handleInvalidCredentials(
+            InvalidCredentialsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
